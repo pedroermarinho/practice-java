f194c6382e8341720382e60f39b5487fd9c1ace2
Pedro Marinho <pedro.marinho238@gmail.com>
2020-10-03 13:53:49 -0400

    aula 19
    Produto e associação muitos para muitos

--- cursomc/src/main/java/io/github/pedroermarinho/cursomc/CursomcApplication.java
+++ cursomc/src/main/java/io/github/pedroermarinho/cursomc/CursomcApplication.java
@@ -9,13 +9,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.github.pedroermarinho.cursomc.domain.Categoria;
+import io.github.pedroermarinho.cursomc.domain.Produto;
 import io.github.pedroermarinho.cursomc.repositories.CategoriaRepository;
+import io.github.pedroermarinho.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -27,7 +31,21 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null,"Informatica");
 		Categoria cat2 = new Categoria(null,"Escritorio");
 		
+		Produto p1 = new Produto(null,"Compuatdor",2000.00);
+		Produto p2 = new Produto(null,"Impressora",800.00);
+		Produto p3 = new Produto(null,"Mouse",80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 	}
 
---
+++ cursomc/src/main/java/io/github/pedroermarinho/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package io.github.pedroermarinho.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import io.github.pedroermarinho.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository  extends JpaRepository<Produto, Integer>{
+
+}
