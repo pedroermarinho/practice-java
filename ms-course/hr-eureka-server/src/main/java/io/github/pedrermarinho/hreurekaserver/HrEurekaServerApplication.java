337cbacaef49cc5c745e62b479b01353ac34707f
Pedro Marinho <pedro.marinho@creathus.org.br>
2022-12-20 14:39:34 -0400

    Configuring Eureka server

--- ms-course/hr-eureka-server/src/main/java/io/github/pedrermarinho/hreurekaserver/HrEurekaServerApplication.java
+++ ms-course/hr-eureka-server/src/main/java/io/github/pedrermarinho/hreurekaserver/HrEurekaServerApplication.java
@@ -2,7 +2,9 @@ package io.github.pedrermarinho.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaServerApplication {
 
