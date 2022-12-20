8ccc5448a3edff8b5ea55d7acf53c1a4a23e0761
Pedro Marinho <pedro.marinho@creathus.org.br>
2022-12-20 11:19:38 -0400

    Project hr-payroll created

---
+++ ms-course/hr-payroll/src/test/java/io/github/pedroermarinho/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pedroermarinho.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
