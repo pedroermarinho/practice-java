ef20c605516f1284fb638cddab8485672a1d0fc2
Pedro Marinho <pedro.marinho@creathus.org.br>
2022-12-20 12:47:18 -0400

    RestTemplate

---
+++ ms-course/hr-payroll/src/main/java/io/github/pedroermarinho/hrpayroll/entities/Worker.java
@@ -0,0 +1,58 @@
+package io.github.pedroermarinho.hrpayroll.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class Worker implements Serializable {
+
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+
+    public Worker() {
+    }
+
+    public Worker(Long id, String name, Double dailyIncome) {
+        this.id = id;
+        this.name = name;
+        this.dailyIncome = dailyIncome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getDailyIncome() {
+        return dailyIncome;
+    }
+
+    public void setDailyIncome(Double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Worker)) return false;
+        Worker worker = (Worker) o;
+        return Objects.equals(getId(), worker.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
--- ms-course/hr-payroll/src/main/java/io/github/pedroermarinho/hrpayroll/services/PaymentService.java
+++ ms-course/hr-payroll/src/main/java/io/github/pedroermarinho/hrpayroll/services/PaymentService.java
@@ -1,12 +1,29 @@
 package io.github.pedroermarinho.hrpayroll.services;
 
 import io.github.pedroermarinho.hrpayroll.entities.Payment;
+import io.github.pedroermarinho.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    private final RestTemplate restTemplate;
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    public PaymentService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     public Payment getPayment(long workerId, int days) {
-        return new Payment("Bob", 200.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", "" + workerId);
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
